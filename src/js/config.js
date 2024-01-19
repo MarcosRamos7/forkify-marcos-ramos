48d4c7b7fd6f159a87f155f1e15bee0c89b22f01
MarcosRamos7 <marcospramos7@gmail.com>
2024-01-19 10:42:41 -0300

    Firts commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '392e44ec-39c6-4464-9b47-a12d618f7b1f';
+export const MODAL_CLOSE_SEC = 2.5;
